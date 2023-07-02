a438c66deb65cc5ddebc5fbce430a625e1434b32
Gunalan27 <gunalan0102@gmail.com>
2023-07-02 14:58:03 +0000

    module conflict solved

--- Angular app/src/app/app.module.ts
+++ Angular app/src/app/app.module.ts
@@ -103,7 +103,7 @@ import { MatIconModule } from '@angular/material/icon';
     AUserComponent,
     UserDetailComponent,
     UserRoleFilterComponent,
-    UserSearchComponent
+    UserSearchComponent,
     EventsComponent,
     ListSearchComponent
   ],
