ff9d5ce6867f482a11681f69b9e264d9c079e5fe
VarshzGithub <varshini02122001@gmail.com>
2023-07-24 16:27:17 +0000

    Added event list, Backend URL pending

--- Angular app/src/app/events/events.component.ts
+++ Angular app/src/app/events/events.component.ts
@@ -1,13 +1,15 @@
 import { Component,Output,EventEmitter, Inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { arrow } from '@popperjs/core';
-import { EventsService } from '../services/event-service/events.service';
-
+import { EventsService } from '../services/api/events.service';
+import { Event } from '../models/event';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
-  styleUrls: ['./events.component.css']
+  styleUrls: ['./events.component.css'],
+  providers:[DatePipe]
 })
 export class EventsComponent {
   @Output() event = new EventEmitter<string>()
@@ -22,31 +24,41 @@ export class EventsComponent {
   searchText: string='';
   temp: any[]=this.events;
 
-  constructor(private router: Router,@Inject(EventsService) private eventser: EventsService) {
+  constructor(private router: Router,@Inject(EventsService) private es: EventsService) {
     
   }
   
   ngOnInit(){
-    this.eventser.getAllEvents().forEach((data)=> this.events.push(data));
-    //console.log(events);
-    //this.events=events;
+    //this.eventser.getAllEvents().forEach((data)=> this.events.push(data));
+  
+    this.es.getEvents().subscribe((Response:Event[])=>{
+      console.log(Response);
+      this.temp=[...Response].filter((data)=>{
+          return data.organiser!=null;
+      });
+      this.events=this.temp;
+  },error=>
+      console.log(error)
+  );
+    console.log(this.events);
   }
 
-  onSearchTexEntered(searchValue: string){
+  onSearchTexEntered(searchValue: string) {
     this.searchText = searchValue.toLowerCase();
-    let array = []
-    //console.log(this.searchText);
-    for(let i=0;i<this.events.length;i++){
-      const eventName = this.events[i].name.toLowerCase();
-      const startDate = this.events[i].start_date.toLowerCase();
-
-
-      if(eventName.includes(this.searchText) || startDate.includes(this.searchText)){
-        array.push(this.events[i]);
-      // console.log(this.events[i].name);
+    const array = [];
+  
+    for (let i = 0; i < this.events.length; i++) {
+      const event = this.events[i];
+      if (event && event.name && event.startDate) {
+        const eventName = event.name.toLowerCase();
+        const startDate = event.startDate.toLowerCase();
+  
+        if (eventName.includes(this.searchText) || startDate.includes(this.searchText)) {
+          array.push(event);
+        }
       }
     }
-
+  
     if (array.length === 0) {
       // No events found
       this.eventsNotFound = true;
@@ -54,9 +66,10 @@ export class EventsComponent {
       // Events found
       this.eventsNotFound = false;
     }
-    console.log(array);
-    this.temp=array;
+  
+    this.temp = array;
   }
+  
   
   redirectToEventDetails(id:any): void {
     console.log(id);
--- Angular app/src/app/services/api/events.service.ts
+++ Angular app/src/app/services/api/events.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AppRESTService } from './appRESTService';
 import { Event } from '../../models/event';
@@ -39,35 +39,66 @@ interface EventInput {
   providedIn: 'root',
 })
 export class EventsService extends AppRESTService {
-  API_URL: string = '/api/events';
+  override API_URL: string;
+  BASE_URL: string = 'https://8080-bebaaaeeaedaddfdebcdfcddfaadfadc.project.examly.io/event';
 
   constructor(private http: HttpClient) {
     super();
   }
   getEvents() {
     return this.http
-      .get<Event[]>(this.API_URL)
-      .pipe(tap(), catchError(this.handleError));
+      .get<Event[]>(this.BASE_URL);
   }
 
-  getEvent(id: number) {
-    return this.http
-      .get<Event>(`${this.API_URL}/${id}`)
-      .pipe(tap(), catchError(this.handleError));
-  }
+  
+ getEvent(id:number){
+      return this.http.get<Event>(this.BASE_URL+'/'+id)
+ }
 
-  getEventsByEventType(type: string) {
-    return this.http
-      .get<Event[]>(`${this.API_URL}?EventType=${type}`)
-      .pipe(tap(), catchError(this.handleError));
-  }
-
-  //Add an event
-  //requires following fields to create
   addEvent(input: EventInput) {
+    const headers = new HttpHeaders().set('Content-Type', 'application/json');
+    console.log("event insert ");
+    
     return this.http
-      .post(`${this.API_URL}`, input)
-      .pipe(tap(), catchError(this.handleError));
+      .post<string>(this.BASE_URL, input, {
+        headers,
+        responseType: 'text' as 'json',
+      })
+      .subscribe(
+        (data) => {
+          console.log(data); 
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+  }
+
+  deleteEvent(id:number){
+    return this.http.delete(this.BASE_URL+"/"+id);
+  }
+
+  updateEvent(id:number,input:any){
+    const headers = new HttpHeaders().set('Content-Type', 'application/json');
+    console.log("event insert ");
+    
+    return this.http
+      .put<string>(this.BASE_URL+"/"+id, input, {
+        headers,
+        responseType: 'text' as 'json',
+      })
+      .subscribe(
+        (data) => {
+          console.log(data); 
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+  }
+  
+  getEventsByOrganiserId(id:number){
+    return this.http.get<Event[]>(`${this.BASE_URL}/organiser/${id}`);
   }
 }
 
