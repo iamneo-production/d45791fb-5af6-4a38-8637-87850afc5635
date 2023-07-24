29d747ad1615d3e58faa4de65c6a11d043f64311
VarshzGithub <varshini02122001@gmail.com>
2023-07-24 16:59:43 +0000

    Updated URL

--- Angular app/src/app/services/auth/auth.service.ts
+++ Angular app/src/app/services/auth/auth.service.ts
@@ -26,7 +26,7 @@ function _window(): any {
   providedIn: 'root',
 })
 export class AuthService {
-  API_URL = 'https://8080-bebaaaeeaedaddfdebcdfcddfaadfadc.project.examly.io';
+  API_URL = 'https://8080-aabbeeedaddfdebcdfcddfaadfadc.project.examly.io';
   JWT = 'jwt';
   USER = 'user';
   // for razorpay to get native window support
